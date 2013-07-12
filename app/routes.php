84ad7b8a1b3cc427afa63c6aab1f134e46176bfb
RiDdLeS <riddles@nucleartoday.com>
2013-07-12 17:11:44 -0500

    route update

--- app/routes.php
+++ app/routes.php
@@ -1,36 +1,27 @@
 <?php
+/**
+ * Non-Secure routes
+ */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+// Landing page
+Route::controller('/', 'HomeController');
 
-
+// Let them logout
 Route::get('logout', function()
 {
     Auth::logout();
     return Redirect::to('/')->with('message', 'You have successfully logged out.');
 });
 
-// Non-Secure routes
-Route::controller('scoreboard', 'ScoreboardController');
-Route::get('/rss', 'ScoreboardController@rss');
-
-// Secure routes
+/**
+ * Secure routes
+ */
 Route::group(array('before' => 'auth'), function()
 {
     Route::controller('admin', 'AdminController');
-	Route::controller('manage', 'ManageController');
     Route::controller('user', 'UserController');
 });
 
-// Landing page
-Route::controller('/', 'HomeController');
+
 
 require_once('start/local.php');
