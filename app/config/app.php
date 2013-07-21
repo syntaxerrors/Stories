938d71a0eda96e7205c32093df216efa75334fa5
RiDdLeS <riddles@nucleartoday.com>
2013-07-21 14:27:25 -0500

    Updating git ignore and adding socket io class

--- app/config/app.php
+++ app/config/app.php
@@ -177,7 +177,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-
+		'SocketIOClient'  => 'ElephantIO\Client'
 	),
 
 );
